7dde0cd33f7b676cbd108304997cbcfb68666c9e
mateuszmarc <mmarckiewicz@gmail.com>
2024-06-14 17:45:29 +0000

    Add dependencies in pom.xml. Create AppConfig and AppInitializer classes Create persistence file.

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,47 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManagerFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Configuration
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableWebMvc
+@EnableTransactionManagement
+public class AppConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(List.of(
+                new MediaType("text", "plain", StandardCharsets.UTF_8)));
+        converters.add(stringConverter);
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("bookCataloguePersistenceUnit");
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+
+}
