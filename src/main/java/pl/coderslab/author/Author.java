5d8d5dcbf5f420a5f179b28587e6b58d1a032406
mateuszmarc <mmarckiewicz@gmail.com>
2024-06-14 18:44:48 +0000

    Create Author entity class

---
+++ src/main/java/pl/coderslab/author/Author.java
@@ -0,0 +1,23 @@
+package pl.coderslab.author;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
