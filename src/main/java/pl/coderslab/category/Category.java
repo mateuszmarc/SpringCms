b7f7bc246a4323b046aee2602219a31cc6262da3
mateuszmarc <mmarckiewicz@gmail.com>
2024-06-14 18:40:50 +0000

    Create Category entity class.

---
+++ src/main/java/pl/coderslab/category/Category.java
@@ -0,0 +1,23 @@
+package pl.coderslab.category;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
